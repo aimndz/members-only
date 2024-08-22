5ba660aec6f8b9133ecc1b9f54b2161d4ac297e7
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-08-22 09:33:57 +0800

    Chore: Update passport importing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 const session = require("express-session");
-const passport = require("./utils/passportConfig"); 
+const passport = require("./config/passport"); 
 
 // Import routers
 const mainRouter = require("./routes/mainRouter");
