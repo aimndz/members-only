c22e8dd0338bd76ea680d81b7e021a9575f23904
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-08-25 22:33:44 +0800

    Chore: Use ejs view layouts

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const path = require("path");
 require("dotenv").config();
 const session = require("express-session");
 const passport = require("./config/passport");
+const ejsLayouts = require("express-ejs-layouts");
 
 // Import routers
 const mainRouter = require("./routes/mainRouter");
 const adminRouter = require("./routes/adminRouter");
 const authRouter = require("./routes/authRouter");
+const setLayout = require("./middleware/setLayout");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(ejsLayouts);
+app.set("layout", "layouts/main");
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(setLayout);
 
 // Initialize session and passport
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
